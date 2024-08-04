1ce22001a454fe86e6af1cdebbc95fb1cd727885
Leo Porto <leonardoreisporto@gmail.com>
2024-08-04 14:58:08 -0300

    exercicios

---
+++ src/Exercícios sobre Union Types no TypeScript/ex5.ts
@@ -0,0 +1,10 @@
+// 5. **Exercício 5:** Declare uma variável `valorOuTexto` que possa ser do tipo `number`, `string`
+// ou `boolean`. Atribua valores de cada tipo e imprima no console.
+let valorOuTexto: string | number | boolean;
+
+valorOuTexto = 10;
+console.log(valorOuTexto);
+valorOuTexto = "10";
+console.log(valorOuTexto);
+valorOuTexto = false;
+console.log(valorOuTexto);
