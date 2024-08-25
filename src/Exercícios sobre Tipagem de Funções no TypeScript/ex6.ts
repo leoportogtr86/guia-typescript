b9d0a6a20faaff3ffc370795b6c474ad1e35615b
Leo Porto <leonardoreisporto@gmail.com>
2024-08-25 15:28:07 -0300

    Exercícios

---
+++ src/Exercícios sobre Tipagem de Funções no TypeScript/ex6.ts
@@ -0,0 +1,5 @@
+// Exercício 6: Crie uma função elevarAoQuadrado que receba um parâmetro do tipo number
+// e retorne o quadrado desse número. Tipifique o parâmetro e o retorno da função.
+const elevarAoQuadrado = (num: number): number => Math.pow(num, 2);
+
+console.log(elevarAoQuadrado(10));
---
+++ src/Exercícios sobre Tipagem de Funções no TypeScript/ex7.ts
@@ -0,0 +1,5 @@
+// Exercício 7: Crie uma função concatenaStrings que receba dois parâmetros do tipo
+// string e retorne a concatenação deles. Tipifique os parâmetros e o retorno da função.
+const concatenaString = (a: string, b: string): string => a + " " + b;
+
+console.log(concatenaString("Leonardo", "Porto"));
